75c8cdd0689ae5158ae9fe803fdb5e5d929161ae
Jani Eväkallio <jani.evakallio@gmail.com>
2016-11-30 10:32:40 +0000

    Check that rsync exists

--- src/commands/run.js
+++ src/commands/run.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const commandExists = require('command-exists');
+
 const whack = require('../util/whack');
 const spawn = require('../util/spawn');
+const log = require('../util/log');
 
 module.exports = function run({ npmScript }) {
 
   const cliConfigPath = path.resolve(__dirname, '../packager/rn-cli.config.js');
 
-  spawn('npm', [npmScript, '--', '--config', cliConfigPath], (code) => {
-    process.exit(code);
-  });
+  commandExists('rsync', (error, rsyncExists) => {
+    if (!error && rsyncExists) {
+      spawn('npm', [npmScript, '--', '--config', cliConfigPath], (code) => {
+        process.exit(code);
+      });
 
-  whack();
+      whack();
+    } else {
+      log.error(
+        'Could\'t find `rsync`. Install it using your favorite package manager and try again'
+      );
+    }
+
+  });
 };
