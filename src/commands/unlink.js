4caa94ba886d1ef611a5d8e0b8ff1aa5aeb77186
Jani Eväkallio <jani.evakallio@gmail.com>
2016-12-01 10:14:41 +0000

    Remove whacked package on unlink

--- src/commands/unlink.js
+++ src/commands/unlink.js
@@ -1,17 +1,29 @@
+const fs = require('fs-extra');
+const path = require('path');
 const assert = require('../util/assert');
 const config = require('../util/config');
 const log = require('../util/log');
-
+const spawn = require('../util/spawn');
 /*
  * Remove a package from whackage.json by name
  */
 module.exports = function unlink({ packageName }) {
   assert.whackageJsonExists();
-  config.update((whackage) => {
-    whackage.dependencies = whackage.dependencies || {};
-    delete whackage.dependencies[packageName];
-    return whackage;
-  });
 
-  log.info(`unlinked ${packageName}`);
+  fs.remove(path.resolve(process.cwd(), 'node_modules', packageName), (error) => {
+    spawn('npm', ['install'], (code) => {
+      config.update((whackage) => {
+        whackage.dependencies = whackage.dependencies || {};
+        delete whackage.dependencies[packageName];
+        return whackage;
+      });
+
+      if (error || code !== 0) {
+        log.error(`unlinked ${packageName}, but your node_modules may not have reset correctly.`);
+        log.error('run `rm -rf node_modules && npm i` to ensure consistent dependencies');
+      } else {
+        log.info(`unlinked ${packageName}`);
+      }
+    });
+  });
 };
